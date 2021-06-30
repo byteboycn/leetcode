dad667d597de5d59bdbcb5ccd595f395ad863172
xhhsc <xhhsc@outlook.com>
2021-06-30 19:55:54 +0800

    编辑距离

---
+++ src/test/java/cn/byteboy/difficulty/hard/_72/EditDistanceTest.java
@@ -0,0 +1,26 @@
+package cn.byteboy.difficulty.hard._72;
+
+import cn.byteboy.core.AbstractTest;
+import org.junit.Test;
+
+/**
+ * @author hongshaochuan
+ * @date 2021/6/30
+ */
+public class EditDistanceTest extends AbstractTest {
+
+    @Test
+    public void test() {
+        verify();
+    }
+
+    @Override
+    protected Object getObj() {
+        return new EditDistance();
+    }
+
+    @Override
+    protected void loadTestCase() {
+        testCaseList.addAll(testCaseScanner.read(""));
+    }
+}
