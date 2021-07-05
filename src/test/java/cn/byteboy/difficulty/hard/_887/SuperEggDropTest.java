9b1e891eb174a9a5f8f1917f7e7c1959e30d0ab7
hongshaochuan <hongshaochuan@newisdom.com.cn>
2021-07-05 18:43:22 +0800

    鸡蛋掉落

---
+++ src/test/java/cn/byteboy/difficulty/hard/_887/SuperEggDropTest.java
@@ -0,0 +1,26 @@
+package cn.byteboy.difficulty.hard._887;
+
+import cn.byteboy.core.AbstractTest;
+import org.junit.Test;
+
+/**
+ * @author hongshaochuan
+ * @Date 2021/7/5
+ */
+public class SuperEggDropTest extends AbstractTest {
+
+    @Test
+    public void test() {
+        System.out.println(verify());
+    }
+
+    @Override
+    protected Object getObj() {
+        return new SuperEggDrop();
+    }
+
+    @Override
+    protected void loadTestCase() {
+        testCaseList.addAll(testCaseScanner.read("TestCases/difficulty/hard/_887"));
+    }
+}
