af55b045e10b2514716b511c2161e133a925ce3a
hongshaochuan <hongshaochuan@newisdom.com.cn>
2021-07-09 13:01:03 +0800

    地下城游戏

---
+++ src/test/java/cn/byteboy/difficulty/hard/_174/DungeonGameTest.java
@@ -0,0 +1,26 @@
+package cn.byteboy.difficulty.hard._174;
+
+import cn.byteboy.core.AbstractTest;
+import org.junit.Test;
+
+/**
+ * @author hongshaochuan
+ * @Date 2021/7/9
+ */
+public class DungeonGameTest extends AbstractTest {
+
+    @Test
+    public void test() {
+        System.out.println(verify());
+    }
+
+    @Override
+    protected Object getObj() {
+        return new DungeonGame();
+    }
+
+    @Override
+    protected void loadTestCase() {
+        testCaseList.addAll(testCaseScanner.read("TestCases/difficulty/hard/_174"));
+    }
+}
