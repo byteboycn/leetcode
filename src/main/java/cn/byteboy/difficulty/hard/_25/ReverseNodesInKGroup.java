20cf5a2dd7fb32522a513da014651da724a7ba08
xhhsc <xhhsc@outlook.com>
2021-07-11 00:15:56 +0800

    K个一组翻转链表（困难）

--- src/main/java/cn/byteboy/difficulty/hard/_25/ReverseNodesInKGroup.java
+++ src/main/java/cn/byteboy/difficulty/hard/_25/ReverseNodesInKGroup.java
@@ -69,7 +69,29 @@ public class ReverseNodesInKGroup {
 
     @Solution
     public ListNode reverseKGroup(ListNode head, int k) {
+        if (head == null) return null;
+        ListNode a, b;
+        a = b = head;
+        for (int i = 0; i < k; i++) {
+            if (b == null) return head;
+            b = b.next;
+        }
+        ListNode newHead = reverse(a, b);
+        // 此时的a已是尾节点
+        a.next = reverseKGroup(b, k);
+        return newHead;
+    }
 
-        return null;
+    private ListNode reverse(ListNode a, ListNode b) {
+        ListNode pre, cur, next;
+        pre = null;
+        cur = a;
+        while (cur != b) {
+            next = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = next;
+        }
+        return pre;
     }
 }
