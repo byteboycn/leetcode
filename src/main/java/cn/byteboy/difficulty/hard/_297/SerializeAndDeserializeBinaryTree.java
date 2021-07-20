f4209202ec14997946189fa1fe0bde446a5e2717
hongshaochuan <hongshaochuan@newisdom.com.cn>
2021-07-20 18:57:16 +0800

    二叉树序列号

---
+++ src/main/java/cn/byteboy/difficulty/hard/_297/SerializeAndDeserializeBinaryTree.java
@@ -0,0 +1,89 @@
+package cn.byteboy.difficulty.hard._297;
+
+import cn.byteboy.core.model.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author hongshaochuan
+ * @Date 2021/7/20
+ *
+ * 序列化是将一个数据结构或者对象转换为连续的比特位的操作，进而可以将转换后的数据存储在一个文件或者内存中，同时也可以通过网络传输到另一个计算机环境，采取相反方
+ * 式重构得到原数据。
+ *
+ *  请设计一个算法来实现二叉树的序列化与反序列化。这里不限定你的序列 / 反序列化算法执行逻辑，你只需要保证一个二叉树可以被序列化为一个字符串并且将这个字符串
+ * 反序列化为原始的树结构。
+ *
+ *  提示: 输入输出格式与 LeetCode 目前使用的方式一致，详情请参阅 LeetCode 序列化二叉树的格式。你并非必须采取这种方式，你也可以采用其他的
+ * 方法解决这个问题。
+ *
+ *
+ *
+ *  示例 1：
+ *
+ *
+ * 输入：root = [1,2,3,null,null,4,5]
+ * 输出：[1,2,3,null,null,4,5]
+ *
+ *
+ *  示例 2：
+ *
+ *
+ * 输入：root = []
+ * 输出：[]
+ *
+ *
+ *  示例 3：
+ *
+ *
+ * 输入：root = [1]
+ * 输出：[1]
+ *
+ *
+ *  示例 4：
+ *
+ *
+ * 输入：root = [1,2]
+ * 输出：[1,2]
+ *
+ *
+ *
+ *
+ *  提示：
+ *
+ *
+ *  树中结点数在范围 [0, 104] 内
+ *  -1000 <= Node.val <= 1000
+ *
+ *  Related Topics 树 深度优先搜索 广度优先搜索 设计 字符串 二叉树
+ *  👍 598 👎 0
+ */
+public class SerializeAndDeserializeBinaryTree {
+
+    private List<String> nodes = new ArrayList<>();
+
+    // 前序遍历
+    // Encodes a tree to a single string.
+    public String serialize(TreeNode root) {
+        traverse(root);
+
+        return "";
+//        return sb.toString();
+    }
+
+    // Decodes your encoded data to tree.
+    public TreeNode deserialize(String data) {
+        return null;
+    }
+
+    private void traverse(TreeNode root) {
+        if (root == null)
+            nodes.add("#");
+        else
+            nodes.add(String.valueOf(root.val));
+
+        traverse(root.left);
+        traverse(root.right);
+    }
+}
