ed68c228f9a505b7cd472909e1065bb58205c29c
xhhsc <xhhsc@outlook.com>
2021-07-31 00:54:47 +0800

    数据流的中位数

---
+++ src/main/java/cn/byteboy/difficulty/hard/_295/MedianFinder.java
@@ -0,0 +1,21 @@
+package cn.byteboy.difficulty.hard._295;
+
+/**
+ * @author hongshaochuan
+ * @date 2021/7/31
+ */
+public class MedianFinder {
+
+    /** initialize your data structure here. */
+    public MedianFinder() {
+
+    }
+
+    public void addNum(int num) {
+
+    }
+
+    public double findMedian() {
+        return 0;
+    }
+}
