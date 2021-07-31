c775813e0bba4ef1277a6d7931d38a72ad35882e
hongshaochuan <hongshaochuan@newisdom.com.cn>
2021-07-31 15:03:19 +0800

    流中找中位数

--- src/main/java/cn/byteboy/difficulty/hard/_295/MedianFinder.java
+++ src/main/java/cn/byteboy/difficulty/hard/_295/MedianFinder.java
@@ -1,21 +1,39 @@
 package cn.byteboy.difficulty.hard._295;
 
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
 /**
  * @author hongshaochuan
  * @date 2021/7/31
  */
 public class MedianFinder {
 
+    private PriorityQueue<Integer> small;
+
+    private PriorityQueue<Integer> large;
+
     /** initialize your data structure here. */
     public MedianFinder() {
-
+        small = new PriorityQueue<>((o1, o2) -> o2 - o1);
+        large = new PriorityQueue<>();
     }
 
     public void addNum(int num) {
-
+        if (small.size() >= large.size()) {
+            small.offer(num);
+            large.offer(small.poll());
+        } else {
+            large.offer(num);
+            small.offer(large.poll());
+        }
     }
 
     public double findMedian() {
-        return 0;
+        if (large.size() < small.size())
+            return small.peek();
+        if (large.size() > small.size())
+            return large.peek();
+        return (large.peek() + small.peek()) / 2.0;
     }
 }
