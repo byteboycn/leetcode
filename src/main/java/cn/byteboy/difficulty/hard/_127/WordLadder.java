015ad6a7a54980a91871a5aaf27445f19cebe21e
xhhsc <xhhsc@outlook.com>
2021-08-12 01:23:45 +0800

    单词接龙

--- src/main/java/cn/byteboy/difficulty/hard/_127/WordLadder.java
+++ src/main/java/cn/byteboy/difficulty/hard/_127/WordLadder.java
@@ -2,7 +2,7 @@ package cn.byteboy.difficulty.hard._127;
 
 import cn.byteboy.core.Solution;
 
-import java.util.List;
+import java.util.*;
 
 /**
  * @author hongshaochuan
@@ -58,12 +58,86 @@ public class WordLadder {
 
     @Solution
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        Set<String> visited = new HashSet<>();
+        Queue<String> q = new LinkedList<>();
+        q.offer(beginWord);
+        int step = 1;   // begin 算一步
+        while (!q.isEmpty()) {
+            int size = q.size();
+            for (int i = 0; i < size; i++) {
+                String cur = q.poll();
+                if (endWord.equals(cur)) {
+                    return step;
+                }
+                for (String s : findTheNext(cur, wordList)) {
+                    if (!visited.contains(s)) {
+                        q.offer(s);
+                        visited.add(s);
+                    }
+
+                }
+            }
+            step++;
+
+        }
 
         return 0;
     }
 
     private List<String> findTheNext(String word, List<String> wordList) {
+        List<String> res = new LinkedList<>();
+        int mark = 0;
+        for (String w : wordList) {
+            for (int i = 0; i < word.length(); i++) {
+                if (w.charAt(i) != word.charAt(i)) {
+                    mark++;
+                    if (mark > 1) {
+                        break;
+                    }
+                }
+            }
+            if (mark == 1)
+                res.add(w);
+            mark = 0;
+        }
+        return res;
+    }
 
-        return null;
+    @Solution("双向BFS")
+    public int ladderLength2(String beginWord, String endWord, List<String> wordList) {
+        if (!wordList.contains(endWord)) return 0;
+        Set<String> q1 = new HashSet<>();
+        Set<String> q2 = new HashSet<>();
+        q1.add(beginWord);
+        q2.add(endWord);
+        int step = 1;
+        Set<String> visited = new HashSet<>();
+
+        while (!q1.isEmpty() && !q2.isEmpty()) {
+            Set<String> temp = new HashSet<>();
+            for (String cur : q1) {
+                if (q2.contains(cur))
+                    return step;
+                visited.add(cur);
+                for (String s : findTheNext(cur, wordList)) {
+                    if (!visited.contains(s)) {
+                        temp.add(s);
+                    }
+                }
+            }
+            step++;
+            q1 = q2;
+            q2 = temp;
+        }
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        List<String> arr = Arrays.asList("hot","dot","dog","lot","log","cog");
+        String beginWord = "hit";
+        String endWord = "cog";
+
+        WordLadder w = new WordLadder();
+        System.out.println(w.ladderLength2(beginWord, endWord, arr));
     }
 }
